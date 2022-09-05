7f8c7718d7bd2e5d0599de29747088114038d106
Aleksey <pilot-1981@yandex.ru>
2022-09-05 17:55:55 +0500

    add DZ_4_1.2

--- DZ_4/Program.cs
+++ DZ_4/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int tmp = a;
+int tmp2 = b;
+while (b > 1)
+{
+    b = b - 1;
+    a = a * tmp;
+}
+Console.WriteLine(tmp + " в степени " + tmp2 + " = " + a);
