2a764581bfc769208a580af98e1169b91eadde2b
Aleksey <pilot-1981@yandex.ru>
2022-09-05 23:28:40 +0500

    add Task_2.2

--- DZ_4/Task_2/Program.cs
+++ DZ_4/Task_2/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿Console.Write("Input a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int num = number;
+int i = 0;
+while (number > 0 && number % 10 != 0)
+{
+    if (num % (number % 10) == 0)
+        i = i + (number % 10);
+    number = number / 10;
+}
+Console.Write(i);
